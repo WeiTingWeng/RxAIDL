2529ddf1398837cc01c56f8e78e58be3bbf4e100
WeiTingWeng <tim@timweng.com>
2018-08-20 14:38:22 +0800

    Fixed the typo

--- rxaidl/src/main/java/com/timweng/lib/rxaidl/exception/ServiceNotSupportedException.kt
+++ rxaidl/src/main/java/com/timweng/lib/rxaidl/exception/ServiceNotSupportedException.kt
@@ -3,4 +3,4 @@ package com.timweng.lib.rxaidl.exception
 import java.lang.Exception
 
 class ServiceNotSupportedException(current: Long, min: Long, max: Long)
-    : Exception("Service is not support: current version is $current, require version is $min to $max")
+    : Exception("Service is not supported: Current version is $current, require version is $min to $max")
